f0c39a73b30d448bca5f7e51908e3e994c029099
Matthias <ma-laux@web.de>
2021-12-07 00:04:30 +0100

    Recipe headline integrated

--- DigitalCookingBook/src/application/RezMainPane.java
+++ DigitalCookingBook/src/application/RezMainPane.java
@@ -27,11 +27,11 @@ public class RezMainPane extends GridPane {
 		getRowConstraints().addAll(row1,row2);
 		add(rez.getInfo(),0,0);
 		add(rez.getZutList(),0,1);
-		add(rez.getImView(),1,0);
+		add(rez.getPAT(),1,0);
 		add(rez.getBeschList(),1,1);
 		setStyle("-fx-background-color: DAE6F3;");
-		GridPane.setHalignment(rez.getInfo(),HPos.CENTER);
-		GridPane.setHalignment(rez.getImView(),HPos.CENTER);
+		//GridPane.setHalignment(rez.getInfo(),HPos.CENTER);
+		//GridPane.setHalignment(rez.getPAT(),HPos.CENTER);
 		GridPane.setHalignment(rez.getZutList(),HPos.CENTER);
 		GridPane.setHalignment(rez.getBeschList(),HPos.CENTER);
 	}
