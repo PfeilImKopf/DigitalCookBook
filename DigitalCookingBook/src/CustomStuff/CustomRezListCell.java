582048a9a0f37a5a377d6bf6c85cb8ecd5994681
Matthias <ma-laux@web.de>
2021-12-08 02:52:43 +0100

    Testing new RecipeList

--- DigitalCookingBook/src/CustomStuff/CustomRezListCell.java
+++ DigitalCookingBook/src/CustomStuff/CustomRezListCell.java
@@ -1,5 +1,7 @@
 package CustomStuff;
 
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.layout.GridPane;
@@ -15,22 +17,23 @@ public class CustomRezListCell extends ListCell<Recipe> {
 	private Label labelName;
 	private GridPane gridPane;
 	public CustomRezListCell() {
-		circle1 = new Circle(7,Color.WHITE);
+		circle1 = new Circle(11,Color.WHITE);
 		circle1.setStroke(Color.BLACK);
-		circle2 = new Circle(7,Color.WHITE);
+		circle2 = new Circle(11,Color.WHITE);
 		circle2.setStroke(Color.BLACK);
-		circle3 = new Circle(7,Color.WHITE);
+		circle3 = new Circle(11,Color.WHITE);
 		circle3.setStroke(Color.BLACK);
 		
 		labelName = new Label();
-		labelName.setFont(new Font(10));
+		GridPane.setHalignment(labelName, HPos.CENTER);
+		labelName.setFont(new Font(15));
 		
 		gridPane = new GridPane();
-		gridPane.add(circle1, 0, 0);
-		gridPane.add(circle2, 1, 0);
-		gridPane.add(circle3, 2, 0);
-		gridPane.add(labelName,3,0);
-		gridPane.setHgap(5);
+		gridPane.add(circle1, 1, 0);
+		gridPane.add(circle2, 3, 0);
+		gridPane.add(circle3, 5, 0);
+		gridPane.add(labelName,0,1,6,1);
+		gridPane.setHgap(12);
 	}
 	@Override
 	protected void updateItem(Recipe rez, boolean empty) {
