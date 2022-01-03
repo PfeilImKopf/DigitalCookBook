e23dd2144c73cdd45665c437381db3bf5183179a
Matthias <ma-laux@web.de>
2022-01-03 03:47:59 +0100

    centeRPane no warnings

--- DigitalCookingBook/src/application/CenterPane.java
+++ DigitalCookingBook/src/application/CenterPane.java
@@ -7,14 +7,14 @@ import recipeAndCategoryPackage.Recipe;
 
 
 public class CenterPane extends BorderPane {
-	private Recipe recipe;
+
 	public CenterPane(Recipe recipe,Stage parent) {
 		TopControlPane topControl = new TopControlPane();
 		setTop(topControl);
 		setCenter(new RecipePane(recipe));
 	}
 	public void setRecipe(Recipe recipe) {
-		this.recipe=recipe;
+
 		setCenter(new RecipePane(recipe));
 	}
 }
