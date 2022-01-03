463ec6f699ca6142909355ed155aca7e52729096
Matthias <ma-laux@web.de>
2022-01-03 03:54:30 +0100

    Instructions no warnings

--- DigitalCookingBook/src/recipeAndCategoryPackage/Instructions.java
+++ DigitalCookingBook/src/recipeAndCategoryPackage/Instructions.java
@@ -3,6 +3,10 @@ package recipeAndCategoryPackage;
 import java.io.Serializable;
 
 public class Instructions implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private final int schrittNum;
 	private String text;
 	
